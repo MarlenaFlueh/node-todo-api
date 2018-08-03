bae92ab635d898325e41614ba53cc3be05797670
MarlenaFlueh <mf@esyoil.com>
2018-08-03 13:51:38 +0200

    add new delete methods

--- playground/mongo-delete.js
+++ playground/mongo-delete.js
@@ -1,4 +1,4 @@
-const MongoClient = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 MongoClient.connect(
   "mongodb://localhost:27017/TodoApp",
@@ -8,9 +8,40 @@ MongoClient.connect(
     }
     console.log("Connection successful");
     const db = client.db("TodoApp");
+    // methods for Todos
 
-    // findOneAndDelete
+    // db.collection("Todos")
+    //   .deleteMany({ text: "eat lunch" })
+    //   .then(result => {
+    //     console.log("Delete Todo");
+    //     console.log(result);
+    //   });
 
+    // db.collection("Todos")
+    //   .deleteOne({ text: "eat lunch" })
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    // db.collection("Todos")
+    //   .findOneAndDelete({ completed: false })
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    // methods for users
+
+    // db.collection("Users")
+    //   .deleteMany({ name: "Marlena" })
+    //   .then(result => {
+    //     console.log(result);
+    //   });
+
+    db.collection("Users")
+      .findOneAndDelete({ _id: new ObjectId("5b64025af3b6232840f4227d") })
+      .then(result => {
+        console.log(JSON.stringify(result, undefined, 2));
+      });
     client.close();
   }
 );
